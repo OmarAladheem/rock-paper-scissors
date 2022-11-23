90ecf458e4c529efe0fec579b6ddbd21757b87cd
Omar Aladheem <omaraladheem@gmail.com>
2022-11-23 01:43:57 +0000

    Added and declare constants for DOM elements and possible choices

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+const buttons = document.getElementsByClassName("control");
+const playerScore = document.getElementById("player-score");
+const computerScore = document.getElementById("computer-score");
+const playerImage = document.getElementById("player-image");
+const computerImage = document.getElementById("computer-image");
+const messages = document.getElementById("messages");
+const choices = ["rock", "paper", "sicssors"];
+const randomChioces = document.getElementById("random");
