0f51a718eff70320380042d1886040a669bc2877
Omar Aladheem <omaraladheem@gmail.com>
2022-11-23 01:56:00 +0000

    Add event listnener to all buttons

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,7 @@
+/**
+ * Declare constans for DOM elements
+ * Add possible choices
+ */
 const buttons = document.getElementsByClassName("control");
 const playerScore = document.getElementById("player-score");
 const computerScore = document.getElementById("computer-score");
@@ -6,3 +10,14 @@ const computerImage = document.getElementById("computer-image");
 const messages = document.getElementById("messages");
 const choices = ["rock", "paper", "sicssors"];
 const randomChioces = document.getElementById("random");
+
+
+/**
+ * Add event listener to all the buttons
+ */
+for (let button of buttons) {
+    button.addEventListener("click", function () {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice);
+    })
+}
